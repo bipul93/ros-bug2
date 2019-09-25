4424c688567b42607576e21d365c0e75ab692d66
bipul93 <kumar.bipulbose@gmail.com>
2019-09-25 17:28:49 -0400

    angle partition mistype issue fix

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import math
 
 import rospy
 from geometry_msgs.msg import PoseStamped
@@ -10,27 +9,31 @@ from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 
 import enum
+import math
 import numpy
 
 
+# Defining Bot States
 class BotState(enum.Enum):
-    LOOK_TOWARDS = 0
-    GOAL_SEEK = 1  # type: BotState
-    WALL_FOLLOW = 2
+    LOOK_TOWARDS = 0  # rotate bots towards the goal
+    GOAL_SEEK = 1  # follow line
+    WALL_FOLLOW = 2  # Go around the wall / avoid obstacles
 
 
 yaw = 0
 yaw_threshold = math.pi / 90
 goal_distance_threshold = 0.25
 currentBotState = BotState.LOOK_TOWARDS
+
+# base scan laser range values
 maxRange = 3
 minRange = 0
 
 bot_pose = None
 init_bot_pose = []
 beacon_pose = None
-bot_motion = None
-homing_signal = None
+bot_motion = None  # cmd_vel publisher
+homing_signal = None  # subscriber
 init_config_complete = False
 wall_hit_point = None
 beacon_found = False
@@ -39,6 +42,8 @@ distance_moved = 0
 
 front_obs_distance = None
 left_obs_distance = None
+
+wall_folllowing = False
 
 
 def normalize(angle):
@@ -55,26 +60,27 @@ def look_towards(des_pos):
         des_pos.orientation.z,
         des_pos.orientation.w)
     euler = euler_from_quaternion(quaternion)
-    yaw = euler[2]
+    yaw = euler[2]  # bot's yaw
     beacon_yaw = math.atan2(beacon_pose.position.y - des_pos.position.y, beacon_pose.position.x - des_pos.position.x)
     yaw_diff = normalize(beacon_yaw - yaw)
 
     if math.fabs(yaw_diff) > yaw_threshold:
         twist.angular.z = -0.5  # clockwise rotation if yaw_diff > 0 else 0.5  # counter-clockwise rotation
-
-    bot_motion.publish(twist)
-
-    if math.fabs(yaw_diff) <= yaw_threshold:
+    else:
+    # if math.fabs(yaw_diff) <= yaw_threshold:
         twist.angular.z = 0
         currentBotState = BotState.GOAL_SEEK
+    bot_motion.publish(twist)
 
 
 def goal_seek():
     global zone_F, currentBotState, bot_pose, wall_hit_point, front_obs_distance, left_obs_distance
     # zone_F = numpy.array(zone_F)
-    obstacle_in_front = numpy.any((zone_F < front_obs_distance))
+    obstacle_in_front = numpy.any((zone_F < 0.85))
+    # obstacle_in_frontLeft = numpy.any((zone_FL <= 0.5))
+    # obstacle_in_frontRight = numpy.any((zone_FR <= 0.5))
     # Or find the minimum value in this zone. or maybe numpy.any would be faster
-    # print(obstacle_in_front)
+    print(obstacle_in_front, zone_F)
     if obstacle_in_front:
         twist.linear.x = 0
         wall_hit_point = bot_pose.position
@@ -94,6 +100,7 @@ def wall_follow():
     obstacle_in_front = numpy.any((zone_F < front_obs_distance))
     distance_moved = math.sqrt(pow(bot_pose.position.y - wall_hit_point.y, 2) + pow(bot_pose.position.x - wall_hit_point.x, 2))
     # print(line_distance(), distance_moved, (line_distance() < 0.2 and distance_moved > 0.5))
+
     if line_distance() < 0.2 and distance_moved > 0.5:
         print("line_hit")
         print(distance_moved)
@@ -101,10 +108,11 @@ def wall_follow():
         twist.angular.z = 0
         twist.linear.x = 0
         currentBotState = BotState.LOOK_TOWARDS
-    elif obstacle_in_front or numpy.any((zone_L < 0.15)):  # turn right
+        return
+    elif obstacle_in_front:  # turn right
         twist.angular.z = -0.5
         twist.linear.x = 0
-    elif numpy.all((zone_FL >= left_obs_distance)):  # turn left TODO: what if there's wall on left and right both sides.
+    elif numpy.all((zone_FL >= left_obs_distance)): #  or numpy.any((zone_FR <= 1)):  # turn left TODO: what if there's wall on left and right both sides.
         twist.angular.z = 0.5
         twist.linear.x = 0
     else:
@@ -112,6 +120,36 @@ def wall_follow():
         twist.linear.x = 0.5
 
     bot_motion.publish(twist)
+
+    # if z_FL <= 1 and z_F > 1 and z_R > 1:
+    #     twist.angular.z = -0.5
+    #     twist.linear.x = 0
+    # elif z_FL > 1 and z_F <= 1 and z_R > 1:
+    #     twist.angular.z = -0.5
+    #     twist.linear.x = 0
+    # elif z_FL > 1 and z_F > 1 and z_R <= 1:
+    #     twist.angular.z = 0.5
+    #     twist.linear.x = 0
+    # elif z_FL <= 1 and z_F <= 1 and z_R > 1:
+    #     twist.angular.z = -0.5
+    #     twist.linear.x = 0
+    # elif z_FL > 1 and z_F <= 1 and z_R <= 1:
+    #     twist.angular.z = 0.5
+    #     twist.linear.x = 0
+    # elif z_FL <= 1 and z_F > 1 and z_R <= 1:
+    #     twist.angular.z = 0
+    #     twist.linear.x = 0.5
+    # elif z_FL <= 1 and z_F <= 1 and z_R <= 1:
+    #     if numpy.any((zone_R > 1)):
+    #         twist.angular.z = -0.5
+    #     elif numpy.any((zone_L > 1)):
+    #         twist.angular.z = 0.5
+    #     else:
+    #         twist.angular.z = 0
+    #     twist.linear.x = 0
+    # else:
+    #     twist.angular.z = 0
+    #     twist.linear.x = 0.5
 
 
 def line_distance():
@@ -149,13 +187,12 @@ def get_base_truth(bot_data):
 
 
 def process_sensor_info(data):
-    global maxRange, minRange, front_obs_distance, left_obs_distance
+    global maxRange, minRange, front_obs_distance, left_obs_distance, zone_R, zone_FR, zone_F, zone_FL, zone_L
     maxRange = data.range_max
     minRange = data.range_min
-    # zone = numpy.array_split(numpy.array(data.ranges), 5)
-    global zone_R, zone_FR, zone_F, zone_FL, zone_L
 
     # Note: Configuration one
+    # zone = numpy.array_split(numpy.array(data.ranges), 5)
     # zone_R = zone[0]
     # zone_FR = zone[1]
     # zone_F = zone[2]
@@ -163,17 +200,17 @@ def process_sensor_info(data):
     # zone_L = zone[4]
     # if front_obs_distance is None and left_obs_distance is None:
     #     front_obs_distance = 0.75
-    #     left_obs_distance = 1
+    #     left_obs_distance = 2
 
     # Note: Configuration 2 - Breaking at uneven angles
     zone = numpy.array(data.ranges)
-    zone_R = zone[0:60]  # 30deg
-    zone_FR = zone[61:140]
-    zone_F = zone[141-220]
-    zone_FL = zone[221:300]
-    zone_L = zone[301-361]
+    zone_R = zone[0:50]  # 30deg
+    zone_FR = zone[51:140]
+    zone_F = zone[141:220]
+    zone_FL = zone[221:310]
+    zone_L = zone[311:361]
     if front_obs_distance is None and left_obs_distance is None:
-        front_obs_distance = 0.75
+        front_obs_distance = 1
         left_obs_distance = 1
 
 
